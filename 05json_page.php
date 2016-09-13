a320927ba78cb823ea3cc245b84466aab18fa74b
YUE HE <may61he@gmail.com>
2016-09-13 19:36:21 -0400

    Update 05json_page.php

--- 05json_page.php
+++ 05json_page.php
@@ -1,10 +1,10 @@
 <?php
 
-	//制作传统分页效果，连接数据库，获得数据，分页显示
+	//create the traditional paging, connect mysql, get the data and show the paging
 	@mysql_connect('192.168.1.107','root','0601') or die(mysql_error());
 	mysql_select_db('data');
 
-	//2:获得总记录数
+	//2:get the total rows number
 	$sql1 = "select * from products";
 	$rs1 = mysql_query($sql1); 
 	$total = mysql_num_rows($rs1);
@@ -13,7 +13,7 @@
 	//3:
 	$pageCount = ceil($total/$per);
 
-	//分页部分，获得传递的当前页码
+	//paging part, get the current pageNo
 	$pageNo = isset($_GET['pageNo'])?$_GET['pageNo']:1;
 	if($pageNo<1){
 		$pageNo=1;
@@ -22,19 +22,18 @@
 		$pageNo=$pageCount;
 	}
 
-	//求当前页的起始位置
+	//get the StartNo
 	$startNo = ($pageNo-1)*$per;
 
-	//获取当前页的内容
+	//get the content of the startNo
 	$sql ="SELECT proID as id,proName as nm,proQuantity as qu,proPrice as pr,proImage as im FROM `products` limit $startNo,$per";   //资源型结果集，里面有好多数据, 我们先把名称变少一点，为了节省带宽
 	$rs = mysql_query($sql);
 
 	$info = array();
 	while($rows=mysql_fetch_assoc($rs)){
-		//$rows 代表每条记录的一维数组信息
-		//要把全部的$rows整合到一起，变为一个二维数组
-		$info[] = $rows;   
+		$info[] = $rows;  //this is two array.
 	}
+	
 	$pagelist = "";
 	for($i=1;$i<=$pageCount;$i++){
 		$pagelist.= "<a href='javascript:showpage(\"./05json_page.php?pageNo={$i}\")'>$i</a>&nbsp;";
@@ -48,13 +47,7 @@
 	//echo $pagelist;
 	$info[] = $pagelist;
 
-	//for($i=1;$i<=$pageCount;$i++){
-		//'<a href="?pageNo='.$i.'">'.$i.'</a>&nbsp;'
-	//}	
-					//echo '<a href="?pageNo='.$i.'">'.$i.'</a>&nbsp;';
-	//$info[] = $pageNo;
-
-	//print_r($info);   //我们发现二维的是索引数组，二维里面的每一个是关联数组
+	//print_r($info);   //index-array and assoc array inside it
 	                  /*Array
 							(
 							    [0] => Array
@@ -76,11 +69,10 @@
 							        )
 							   ）  
 						*/
-	echo json_encode($info);  //最外面是数组，数组里面每个是json对象
+	echo json_encode($info);  //[{},{},{},{},{}]
 
 
 
-		//需要把全部的信息整合一下
-		//json_encode（）只能使用一次	
+	//we need to combine all the information then use only one time json_encode（）
 ?>
 
